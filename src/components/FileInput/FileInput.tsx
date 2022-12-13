3e1cee4000ce2b6667af8a783988befd55563e6c
kamil-homernik <kamil.homernik@bayer.com>
2022-12-13 13:30:09 +0100

    added error list

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -4,7 +4,7 @@ import styles from "./FileInput.module.scss";
 import classNames from "classnames";
 import { getFileUrl } from "../../utils/helpers";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-
+import { Grid } from "@mui/material";
 interface FileInputProps {
   setFiles: (file: any) => void;
 }
@@ -16,17 +16,35 @@ function FileInput({ setFiles }: FileInputProps) {
     },
     [setFiles]
   );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop,
-    accept: {
-      "image/*": [".jpeg", ".png"],
-    },
-    maxFiles: 1,
-  });
+  const { getRootProps, getInputProps, fileRejections, isDragActive } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "image/jpeg": [],
+        "image/png": [],
+      },
+      maxFiles: 1,
+    });
+
+  let errorsList: string[] = [];
+
+  errorsList = fileRejections.reduce((errors, fileRejection) => {
+    fileRejection.errors.forEach((error) => {
+      if (!errors.includes(error.message)) {
+        errors = [...errors, error.message];
+      }
+    });
+
+    return errors;
+  }, errorsList);
 
   const fileInputClassName = classNames(styles.fileInput, {
     [styles.fileInput_active]: isDragActive,
   });
+
+  const renderErrorMessages = errorsList.map((error) => (
+    <div key={error}>{error}</div>
+  ));
 
   const inputLabel = isDragActive ? (
     <div className={styles.inputLabel}>Drop the files here ...</div>
@@ -36,12 +54,18 @@ function FileInput({ setFiles }: FileInputProps) {
       <p>Drag 'n' drop some files here, or click to select files</p>
     </div>
   );
-
   return (
-    <div {...getRootProps()} className={fileInputClassName}>
-      <input {...getInputProps()} />
-      {inputLabel}
-    </div>
+    <Grid container>
+      <Grid container item xs={12} justifyContent="center" alignItems="center">
+        <div {...getRootProps()} className={fileInputClassName}>
+          <input {...getInputProps()} />
+          {inputLabel}
+        </div>
+      </Grid>
+      <Grid container item xs={12} justifyContent="center" alignItems="center">
+        {renderErrorMessages}
+      </Grid>
+    </Grid>
   );
 }
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,2 +1,18 @@
+import { FileRejection } from "react-dropzone";
+
 export const getFileUrl = (file: any) =>
   Object.assign(file, { preview: URL.createObjectURL(file) });
+
+export const prepareErrorList = (
+  fileRejections: FileRejection[],
+  initialArray: string[]
+) =>
+  fileRejections.reduce((errors, fileRejection) => {
+    fileRejection.errors.forEach((error) => {
+      if (!errors.includes(error.message)) {
+        errors = [...errors, error.message];
+      }
+    });
+
+    return errors;
+  }, initialArray);
